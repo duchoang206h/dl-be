a15020f01351a04c50c14316d4c61ec1587a1ad6
Hồ Đức Hoàng <duchh@vdcs.vn>
2023-02-06 00:48:03 +0700

    fix: get flag

--- src/utils/country.js
+++ src/utils/country.js
@@ -1,4 +1,4 @@
-const getFlag = (country) => process.env.APP_URL + '/static/images/' + country.toLowerCase() + '.png';
+const getFlag = (country) => process.env.APP_URL + '/static/images/country_' + country.toLowerCase() + '.png';
 module.exports = {
   getFlag,
 };
