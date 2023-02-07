ae0467eb32832334dc7dd323471bc561f8a6d02e
Hồ Đức Hoàng <duchh@vdcs.vn>
2023-02-07 13:05:04 +0700

    fix: gofler button

--- src/services/livestream.service.js
+++ src/services/livestream.service.js
@@ -616,7 +616,7 @@ const getGolferBottom = async ({ code, courseId }) => {
   const response = {};
   const [course, player, images] = await Promise.all([
     Course.findByPk(courseId),
-    Player.findOne({ where: { code } }),
+    Player.findOne({ where: { code, course_id: courseId } }),
     Image.findAll({
       where: {
         course_id: courseId,
