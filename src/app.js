bee7d8bf103d21c2e7902964bc4c86e971fa6c94
Hồ Đức Hoàng <duchh@vdcs.vn>
2023-02-07 13:50:40 +0700

    fix :cors

--- src/app.js
+++ src/app.js
@@ -28,12 +28,12 @@ if (config.env !== 'test') {
 }
 
 // set security HTTP headers
-app.use(helmet());
+/* app.use(helmet());
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});
+}); */
 // parse json request body
 app.use(express.json());
 
