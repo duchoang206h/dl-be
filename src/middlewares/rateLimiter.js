e4909dfb8335b0b64baeae487deebcf4cefb90b3
Hồ Đức Hoàng <duchh@vdcs.vn>
2023-02-08 09:09:30 +0700

    fix 429

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 5000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false,
 });
