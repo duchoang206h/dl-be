33505e75466e32e2ff265cbd5b6d07d06ecd01d7
Hồ Đức Hoàng <duchh@vdcs.vn>
2023-02-09 15:07:12 +0700

    fix download country images

--- src/models/country.js
+++ src/models/country.js
@@ -7,13 +7,23 @@ const downloadCountryImages = async () => {
   try {
     for (const country of COUNTRY) {
       await delay(100);
-      const result = await axios.get(`https://countryflagsapi.com/png/${country.code.toLowerCase()}`, {
-        responseType: 'stream',
-      });
+      try {
+        const result = await axios.get(`https://countryflagsapi.com/png/${country.code.toLowerCase()}`, {
+          responseType: 'stream',
+        });
 
-      result.data.pipe(
-        fs.createWriteStream(path.join(__dirname, '../../data/images', `country_${country.code.toLowerCase()}.png`))
-      );
+        result.data.pipe(
+          fs.createWriteStream(path.join(__dirname, '../../data/images', `country_${country.code.toLowerCase()}.png`))
+        );
+      } catch (error) {
+        const result = await axios.get(`https://countryflagsapi.com/png/${country.code.toLowerCase()}`, {
+          responseType: 'stream',
+        });
+
+        result.data.pipe(
+          fs.createWriteStream(path.join(__dirname, '../../data/images', `country_${country.code.toLowerCase()}.png`))
+        );
+      }
     }
   } catch (error) {
     console.log(error);
