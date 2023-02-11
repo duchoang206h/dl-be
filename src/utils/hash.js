8e3d68cdb596e4afae6f0b0f4b47b03fe1772ab4
Hồ Đức Hoàng <duchh@vdcs.vn>
2023-02-12 00:11:20 +0700

    fix: get score images

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 const saltRound = 10;
 const hashPassword = (password) => bcrypt.hashSync(password, saltRound);
 const comparePassword = (password, hash) => bcrypt.compareSync(password, hash);
-console.log(hashPassword('123456'));
+console.log(hashPassword('t&zt!zbt'));
 module.exports = {
   hashPassword,
   comparePassword,
