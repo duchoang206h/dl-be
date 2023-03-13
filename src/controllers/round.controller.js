eec1ffe5ebee020b6ee2a997f1011c9dfb61d7ed
Thanh-Tran <thanhtv@vdcs.vn>
2023-03-13 15:22:15 +0700

    fix update finshed round

--- src/controllers/round.controller.js
+++ src/controllers/round.controller.js
@@ -1,14 +1,13 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
-const { Round, MatchPlayVersus } = require('../models/schema');
+const { Round, MatchPlayVersus, PlayerFinishRound } = require('../models/schema');
 const { courseService } = require('../services');
 const { COURSE_TYPE } = require('../config/constant');
 const updateRound = catchAsync(async (req, res) => {
   const { courseId, roundNum } = req.params;
-  const { finished, matchNum, playerId } = req.body;
+  const { finished, matchNum, player_id } = req.body;
   const course = await courseService.getCourseById(courseId);
   if (course.type === COURSE_TYPE.MATCH_PLAY) {
-    console.log({ course_id: courseId, round_num: roundNum, match_num: matchNum });
     await MatchPlayVersus.update(
       { finished },
       {
@@ -20,6 +19,13 @@ const updateRound = catchAsync(async (req, res) => {
       }
     );
   } else if (course.type === COURSE_TYPE.STOKE_PLAY) {
+    await PlayerFinishRound.upsert(
+      {
+        round_num: roundNum,
+        finished,
+        player_id,
+      }
+    )
   }
   res.status(httpStatus.OK).send();
 });
